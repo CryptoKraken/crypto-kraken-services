4b27ae92930f38d100040c316c24a7d6072d42cc
Andrey Skubarenko <nuts11971@gmail.com>
2018-03-15 18:55:18 +0300

    Replace RepeatPromise to Promise in methods of the ExchangeService interface

--- src/core/exchange-service.ts
+++ src/core/exchange-service.ts
@@ -1,18 +1,17 @@
 import { CurrencyPair } from './currency-pair';
 import { Order, OrderInfo } from './order';
 import { OrderBook } from './order-book';
-import { RepeatPromise } from '../utils';
 
 export interface ExchangeService {
     // Orders
-    getOrderBook(pair: CurrencyPair): RepeatPromise<OrderBook>;
-    getRecentDealOrders(pair: CurrencyPair, maxLimit?: number): RepeatPromise<Order[]>;
+    getOrderBook(pair: CurrencyPair): Promise<OrderBook>;
+    getRecentDealOrders(pair: CurrencyPair, maxLimit?: number): Promise<Order[]>;
 
     // Account orders
-    createOrder(order: Order): RepeatPromise<Order & { id: string }>;
-    deleteOrder(id: string): RepeatPromise<boolean>;
-    getOrderInfo(id: string): RepeatPromise<OrderInfo>;
-    getActiveOrders(): RepeatPromise<Order & { id: string }[]>;
+    createOrder(order: Order): Promise<Order & { id: string }>;
+    deleteOrder(id: string): Promise<boolean>;
+    getOrderInfo(id: string): Promise<OrderInfo>;
+    getActiveOrders(): Promise<Order & { id: string }[]>;
 
-    getBalance(currency: string): RepeatPromise<number>;
+    getBalance(currency: string): Promise<number>;
 }
