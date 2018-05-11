998a172f5d7a199681bb870fe124125da5b84c1a
Andrey Skubarenko <nuts11971@gmail.com>
2018-05-11 17:36:45 +0300

    Rename the kucoin.ts to the kucoin-service.ts

--- src/services/kucoin/index.ts
+++ src/services/kucoin/index.ts
@@ -1,10 +1,10 @@
-export { KuCoinService } from './kucoin';
+export { KuCoinService } from './kucoin-service';
 
 
 import {
     KUCOIN_DEFAULT_RECENTLY_DEAL_ORDERS_URI,
     KUCOIN_DEFAULT_SERVER_PRODUCTION_URI
-} from './kucoin';
+} from './kucoin-service';
 export const InternalConstants = {
     KUCOIN_DEFAULT_RECENTLY_DEAL_ORDERS_URI,
     KUCOIN_DEFAULT_SERVER_PRODUCTION_URI
