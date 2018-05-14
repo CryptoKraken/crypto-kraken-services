235776ce0bb3e04bdc4da28a88b13db57b6dc92c
Andrey Skubarenko <nuts11971@gmail.com>
2018-05-14 14:54:23 +0300

    Add a test for with getting a full order book and connection error

--- tests/services/kucoin/kucoin-service.spec.ts
+++ tests/services/kucoin/kucoin-service.spec.ts
@@ -32,21 +32,24 @@ describe('KuCoin Exchange Service', () => {
         const orders1 = await kuCoinResponse.getRecentDealOrders(currencyPair);
         const orders2 = await kuCoinResponse.getRecentDealOrders(currencyPair);
 
+        expect(orders1.length)
+            .to.eql(orders2.length)
+            .to.eql(currentCase.data.length);
         expect(orders1).to.eql(orders2);
         expect(orders1[0].amount).to.eql(currentCase.data[0][3]);
         expect(orders1[0].orderType).to.eql(OrderType.Sell);
-        expect(currentCase.data[0][1]).to.eql('SELL');
-        expect(orders1[0].pair).to.eql(currencyPair);
         expect(orders1[0].price).to.eql(currentCase.data[0][2]);
+        expect(orders1[0].pair).to.eql(currencyPair);
+        expect(currentCase.data[0][1]).to.eql('SELL');
 
         expect(orders2[1].amount).to.eql(currentCase.data[1][3]);
+        expect(orders2[1].price).to.eql(currentCase.data[1][2]);
+        expect(orders2[1].pair).to.eql(currencyPair);
         expect(orders2[1].orderType).to.eql(OrderType.Sell);
         expect(currentCase.data[1][1]).to.eql('SELL');
-        expect(orders2[1].pair).to.eql(currencyPair);
-        expect(orders2[1].price).to.eql(currentCase.data[1][2]);
     });
 
-    it('should get recent deal orders despite the error connection', async () => {
+    it('should get recent deal orders despite the connection error', async () => {
         const currentCase = recentDealOrdersData[0];
         const currencyPair: CurrencyPair = ['AAA', 'BBB'];
 
@@ -55,7 +58,7 @@ describe('KuCoin Exchange Service', () => {
             .query({
                 symbol: `${currencyPair[0]}-${currencyPair[1]}`
             })
-            .replyWithError('Error');
+            .replyWithError('An connection error from the test');
         nock(KUCOIN_SERVER_PRODUCTION_URI)
             .get(KUCOIN_RECENTLY_DEAL_ORDERS_URI)
             .query({
@@ -64,6 +67,12 @@ describe('KuCoin Exchange Service', () => {
             .reply(200, currentCase);
 
         const orders = await kuCoinResponse.getRecentDealOrders(currencyPair);
+
+        expect(orders[1].amount).to.eql(currentCase.data[1][3]);
+        expect(orders[1].pair).to.eql(currencyPair);
+        expect(orders[1].price).to.eql(currentCase.data[1][2]);
+        expect(orders[1].orderType).to.eql(OrderType.Sell);
+        expect(currentCase.data[1][1]).to.eql('SELL');
     });
 
     it('should get a full order book', async () => {
@@ -85,7 +94,7 @@ describe('KuCoin Exchange Service', () => {
             .to.eql(orderBook2.buyOrders.length)
             .to.eql(currentCase.data.BUY.length);
         expect(orderBook1.sellOrders.length)
-        expect(orderBook2.sellOrders.length)
+            .to.eql(orderBook2.sellOrders.length)
             .to.eql(currentCase.data.SELL.length);
 
         expect(orderBook1.buyOrders[1].amount).to.eql(currentCase.data.BUY[1][1]);
@@ -107,5 +116,40 @@ describe('KuCoin Exchange Service', () => {
         expect(orderBook2.sellOrders[1].orderType).to.eql(OrderType.Sell);
         expect(orderBook2.sellOrders[1].pair).to.eql(currencyPair);
         expect(orderBook2.sellOrders[1].price).to.eql(currentCase.data.SELL[1][0]);
+    });
+
+    it('should get a full order book despite the connection error', async () => {
+        const currentCase = fullOrderBookData[0];
+        const currencyPair: CurrencyPair = ['AAA', 'BBB'];
+
+        nock(KUCOIN_SERVER_PRODUCTION_URI)
+            .get(KUCOIN_ORDER_BOOKS_URI)
+            .query({
+                symbol: `${currencyPair[0]}-${currencyPair[1]}`
+            })
+            .replyWithError('An connection error from the test');
+        nock(KUCOIN_SERVER_PRODUCTION_URI)
+            .get(KUCOIN_ORDER_BOOKS_URI)
+            .query({
+                symbol: `${currencyPair[0]}-${currencyPair[1]}`
+            })
+            .reply(200, currentCase);
+
+        const orderBook = await kuCoinResponse.getOrderBook(currencyPair);
+
+        expect(orderBook.buyOrders.length)
+            .to.eql(currentCase.data.BUY.length);
+        expect(orderBook.sellOrders.length)
+            .to.eql(currentCase.data.SELL.length);
+
+        expect(orderBook.buyOrders[0].amount).to.eql(currentCase.data.BUY[0][1]);
+        expect(orderBook.buyOrders[0].orderType).to.eql(OrderType.Buy);
+        expect(orderBook.buyOrders[0].pair).to.eql(currencyPair);
+        expect(orderBook.buyOrders[0].price).to.eql(currentCase.data.BUY[0][0]);
+
+        expect(orderBook.sellOrders[0].amount).to.eql(currentCase.data.SELL[0][1]);
+        expect(orderBook.sellOrders[0].orderType).to.eql(OrderType.Sell);
+        expect(orderBook.sellOrders[0].pair).to.eql(currencyPair);
+        expect(orderBook.sellOrders[0].price).to.eql(currentCase.data.SELL[0][0]);
     });
 });
