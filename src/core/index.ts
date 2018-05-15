9a6ed442c606220daa6e9123983b0b87e8aa96ac
Andrey Skubarenko <nuts11971@gmail.com>
2018-05-15 18:41:09 +0300

    Add the exchange credentails. Expand the ExcnangeService interface

--- src/core/index.ts
+++ src/core/index.ts
@@ -2,3 +2,4 @@ export { CurrencyPair } from './currency-pair';
 export { Order, OrderInfo, OrderType } from './order';
 export { OrderBook } from './order-book';
 export { ExchangeService } from './exchange-service';
+export { ExchangeCredentials } from './exchange-credentials';
