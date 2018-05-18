dcbfe3e9deaab2f8aa089dd5b46d85d1d94308d9
maxima-net <maxima-net@bk.ru>
2018-05-19 01:46:36 +0300

    Fix yobit service test

--- tests/services/yobit/yobit-service.spec.ts
+++ tests/services/yobit/yobit-service.spec.ts
@@ -1,12 +1,12 @@
 import { YobitService } from '../../../src/services/yobit';
 import * as nock from 'nock';
-import { YOBIT_CONSTANTS } from '../../../src/services/yobit/constants';
+import { YobitConstants } from '../../../src/services/yobit/constants';
 import { orderBookRawResponse, orderBookParsed } from './data';
 import { expect } from 'chai';
 import { OrderType, OrderBook } from '../../../src/core';
 
 describe('YoBit Exchange Service', () => {
-    const getOrderBookUrlPostfix = `/${YOBIT_CONSTANTS.GET_ORDER_BOOK_METHOD_NAME}/ltc_btc`;
+    const getOrderBookUrlPostfix = `/${YobitConstants.GET_ORDER_BOOK_METHOD_NAME}/ltc_btc`;
     let exchangeService: YobitService;
 
     beforeEach(() => {
@@ -14,7 +14,7 @@ describe('YoBit Exchange Service', () => {
     });
 
     it('should get an order book', async () => {
-        nock(YOBIT_CONSTANTS.ROOT_API_URL)
+        nock(YobitConstants.ROOT_API_URL)
             .get(getOrderBookUrlPostfix)
             .reply(200, JSON.stringify(orderBookRawResponse));
 
@@ -22,7 +22,7 @@ describe('YoBit Exchange Service', () => {
         expect(result).to.be.eql(orderBookParsed);
     });
     it('should get an order book with the second request (the first request causes an error)', async () => {
-        nock(YOBIT_CONSTANTS.ROOT_API_URL)
+        nock(YobitConstants.ROOT_API_URL)
             .get(getOrderBookUrlPostfix)
             .reply(500)
             .get(getOrderBookUrlPostfix)
@@ -32,7 +32,7 @@ describe('YoBit Exchange Service', () => {
         expect(result).to.be.eql(orderBookParsed);
     });
     it('should pass query parameters for getting an order book with applied maxLimit', async () => {
-        nock(YOBIT_CONSTANTS.ROOT_API_URL)
+        nock(YobitConstants.ROOT_API_URL)
             .get(getOrderBookUrlPostfix)
             .query({ limit: 4 })
             .reply(200, JSON.stringify(orderBookRawResponse));
