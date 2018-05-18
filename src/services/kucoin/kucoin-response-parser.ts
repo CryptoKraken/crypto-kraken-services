c58439730732b342665d107adcd220539ed3d569
Andrey Skubarenko <nuts11971@gmail.com>
2018-05-19 01:47:36 +0300

    Use PascalCase for the types of the KuCoinResponceParser. Rename guards methods.

--- src/services/kucoin/kucoin-response-parser.ts
+++ src/services/kucoin/kucoin-response-parser.ts
@@ -1,18 +1,18 @@
 import { isArray, isNumber, isString } from 'util';
 import { Order, OrderType, CurrencyPair, OrderBook } from '../../core';
 
-type orderResponseResult = [
+type OrderResponseResult = [
     /*Price*/ number,
     /*Amount*/ number,
     /*Volume*/ number
 ];
 
-type orderBookResponseResult = {
-    'SELL': orderResponseResult[],
-    'BUY': orderResponseResult[]
+type OrderBookResponseResult = {
+    'SELL': OrderResponseResult[],
+    'BUY': OrderResponseResult[]
 };
 
-type dealOrderResponseResult = [
+type DealOrderResponseResult = [
     /*Timestamp*/ number,
     /*Order Type*/ string,
     /*Price*/ number,
@@ -21,28 +21,28 @@ type dealOrderResponseResult = [
 ];
 
 const Guards = {
-    dataObjOwnerGuard: (dataOwner: any): dataOwner is { data: any } => dataOwner && dataOwner.data,
-    dataArrayOwnerGuard: (dataOwner: any): dataOwner is { data: any[] } => Guards.dataObjOwnerGuard(dataOwner) && isArray(dataOwner.data),
-    orderBookGuard: (orderBook: any): orderBook is orderBookResponseResult => {
+    isDataObjOwner: (dataOwner: any): dataOwner is { data: any } => dataOwner && dataOwner.data,
+    isDataArrayOwner: (dataOwner: any): dataOwner is { data: any[] } => Guards.isDataObjOwner(dataOwner) && isArray(dataOwner.data),
+    isOrderBook: (orderBook: any): orderBook is OrderBookResponseResult => {
         return orderBook && orderBook['SELL'] && orderBook['BUY']
             && isArray(orderBook['SELL']) && isArray(orderBook['BUY'])
-            && orderBook['SELL'].every((order: any) => Guards.orderGuard(order))
-            && orderBook['BUY'].every((order: any) => Guards.orderGuard(order));
+            && orderBook['SELL'].every((order: any) => Guards.isOrder(order))
+            && orderBook['BUY'].every((order: any) => Guards.isOrder(order));
     },
-    orderGuard: (order: any): order is orderResponseResult => {
+    isOrder: (order: any): order is OrderResponseResult => {
         return order && isNumber(order[0]) && isNumber(order[1]) && isNumber(order[2]);
     },
-    dealOrderGuard: (dealOrder: any): dealOrder is dealOrderResponseResult => {
+    isDealOrder: (dealOrder: any): dealOrder is DealOrderResponseResult => {
         return dealOrder && isNumber(dealOrder[0]) && isString(dealOrder[1])
             && isNumber(dealOrder[2]) && isNumber(dealOrder[3]) && isNumber(dealOrder[4]);
     },
-    orderTypeGuard: (orderType: string): orderType is 'SELL' | 'BUY' => orderType === 'SELL' || orderType === 'BUY'
+    isOrderType: (orderType: string): orderType is 'SELL' | 'BUY' => orderType === 'SELL' || orderType === 'BUY'
 };
 
 export class KuCoinResponseParser {
     parseOrderBook(currencyPair: CurrencyPair, responseResult: string): OrderBook {
         const obj = JSON.parse(responseResult);
-        if (!Guards.dataObjOwnerGuard(obj) || !Guards.orderBookGuard(obj.data))
+        if (!Guards.isDataObjOwner(obj) || !Guards.isOrderBook(obj.data))
             throw new Error(`The result ${responseResult} isn't the order book type.`);
         const orderBookResponseResult = obj.data;
         return {
@@ -69,13 +69,13 @@ export class KuCoinResponseParser {
 
     parseDealOrders(currencyPair: CurrencyPair, responseResult: string): Order[] {
         const obj = JSON.parse(responseResult);
-        if (!Guards.dataArrayOwnerGuard(obj))
+        if (!Guards.isDataArrayOwner(obj))
             throw new Error(`The result ${responseResult} isn't the orders type.`);
 
         return obj.data.map<Order>(orderObj => {
-            if (!Guards.dealOrderGuard(orderObj))
+            if (!Guards.isDealOrder(orderObj))
                 throw new Error(`The element (${orderObj}) of the orders object isn't the deal order type.`);
-            if (!Guards.orderTypeGuard(orderObj[1]))
+            if (!Guards.isOrderType(orderObj[1]))
                 throw new Error(`The type of the order (${orderObj}) is incorrect. It should be 'SELL' or 'BUY' value.`);
 
             return {
