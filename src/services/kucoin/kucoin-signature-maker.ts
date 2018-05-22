7b5aefb2c615804bc99aa341b1fdd055780352db
Andrey Skubarenko <nuts11971@gmail.com>
2018-05-23 00:33:19 +0300

    Add some tests for the kucoin signature maker

--- src/services/kucoin/kucoin-signature-maker.ts
+++ src/services/kucoin/kucoin-signature-maker.ts
@@ -1,14 +1,14 @@
-import * as crypto from 'crypto';
-import { Buffer } from 'buffer';
+import createHmac = require('create-hmac');
+import { Buffer } from 'safe-buffer';
 
 export class KuCoinSignatureMaker {
-    sign(secretKey: string, apiEndpoint: string, queryString: string | undefined, timestamp = Date.now()) {
+    sign(secretKey: string, apiEndpoint: string, queryString: string | undefined, timestamp = Date.now()): string {
         let stringForSign = `${apiEndpoint}/${timestamp}/`;
         if (queryString)
             stringForSign += `${queryString}`;
 
         const signatureString = new Buffer(stringForSign).toString('base64');
-        return crypto.createHmac('sha256', secretKey)
+        return createHmac('sha256', secretKey)
             .update(signatureString)
             .digest('hex');
     }
