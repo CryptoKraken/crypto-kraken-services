c7f55888dae4ba1bca562d4982dad1033b561201
Maxim Kucherov <maxima-net@bk.ru>
2018-06-12 19:14:28 +0300

    Use qs for YobitSignatureMaker's sign method

--- src/services/yobit/yobit-signature-maker.ts
+++ src/services/yobit/yobit-signature-maker.ts
@@ -1,16 +1,12 @@
 import createHmac = require('create-hmac');
+import * as qs from 'qs';
 
 export class YobitSignatureMaker {
     sign(
         secretKey: string,
         params: { [key: string]: string | number },
     ): string {
-        const paramsForSignArray: string[] = [];
-        for (const key in params)
-            if (params.hasOwnProperty(key))
-                paramsForSignArray.push(`${key}=${params[key]}`);
-
-        const paramsForSignString = `${paramsForSignArray.join('&')}`;
+        const paramsForSignString = qs.stringify(params, { addQueryPrefix: false });
         return createHmac('sha512', secretKey)
             .update(paramsForSignString)
             .digest('hex');
