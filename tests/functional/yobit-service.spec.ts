ee005af9b7082467fe76327ad939ebfdadd13469
Maxim Kucherov <maxima-net@bk.ru>
2018-06-18 00:33:51 +0300

    Add functional tests for the Yobit's getOrderBook method

---
+++ tests/functional/yobit-service.spec.ts
@@ -0,0 +1,31 @@
+import { expect } from 'chai';
+import { YobitExchangeCredentials, YobitService } from '../../src';
+import { testsConfig } from './tests.config';
+
+describe('The Yobit service', () => {
+    let service: YobitService;
+    let credentials: YobitExchangeCredentials;
+
+    before(() => {
+        service = new YobitService();
+        service.requestTryCount = 0;
+        credentials = {
+            apiKey: testsConfig.exchangeCredentials.yobit.apiKey,
+            secret: testsConfig.exchangeCredentials.yobit.secret
+        };
+    });
+
+    it('should get an order book correctly', async () => {
+        const orderBook = await service.getOrderBook(['eth', 'btc']);
+
+        expect(orderBook.buyOrders.length).to.eql(150);
+        expect(orderBook.sellOrders.length).to.eql(150);
+    });
+
+    it('should get an order book with max limit equaling 1', async () => {
+        const orderBook = await service.getOrderBook(['eth', 'btc'], 1);
+
+        expect(orderBook.buyOrders.length).to.eql(1);
+        expect(orderBook.sellOrders.length).to.eql(1);
+    });
+});
