c0c349567e69aa1f7e340a74cae2032235d459ff
Andrey Skubarenko <nuts11971@gmail.com>
2018-06-20 20:23:16 +0300

    Add core types

--- packages/crypto-kraken-kucoin-api/src/core.ts
+++ packages/crypto-kraken-kucoin-api/src/core.ts
@@ -2,3 +2,22 @@ export interface CurrencyPair {
     0: string;
     1: string;
 }
+
+export type DeepReadonly<T> = {
+    readonly [P in keyof T]: T[P] extends Array<infer U> ? ReadonlyArray<DeepReadonly<U>> : DeepReadonly<T[P]>;
+};
+
+export type DeepPartial<T> = {
+    [P in keyof T]?: DeepPartial<T[P]>;
+};
+
+export type GuardFieldsSelector<T> = {
+    [P in keyof T]: T[P] extends Array<infer U> ? true : T[P] extends object ? GuardFieldsSelector<T[P]> : true;
+};
+
+export type GuardFieldsSelectorMapper<Type, Selector> = {
+    [P in keyof Type]: P extends keyof Selector ? GuardFieldsSelectorMapper<Type[P], Selector[P]> : any;
+};
+
+export type GuardResult<Type, Selector extends DeepPartial<GuardFieldsSelector<Type>>> =
+    GuardFieldsSelectorMapper<Type, Selector>;
