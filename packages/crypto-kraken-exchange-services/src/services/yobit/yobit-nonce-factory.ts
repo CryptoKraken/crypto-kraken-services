35533320b9522405e93c5b577c3bba188e6e8348
Maxim Kucherov <maxima-net@bk.ru>
2018-06-21 12:04:57 +0300

    Fix the yobitNonceFactory

--- packages/crypto-kraken-exchange-services/src/services/yobit/yobit-nonce-factory.ts
+++ packages/crypto-kraken-exchange-services/src/services/yobit/yobit-nonce-factory.ts
@@ -1,1 +1,7 @@
-export const yobitNonceFactory = () => Date.now() / 1000;
+// The Yobit restrictions (from https://yobit.net/en/api/):
+// (1 minimum to 2147483646 maximum) in succeeding request should exceed that in the previous one.
+// To null nonce it is necessary to generate new key.
+
+const offset = 1529330410000;
+// Allows generating a new nonce for every 100 milliseconds.
+export const yobitNonceFactory = () => (Date.now() - offset) / 100;
