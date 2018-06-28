f71cfda65a4e0c1cd59a555c35b73eecc3e14a4e
Andrey Skubarenko <nuts11971@gmail.com>
2018-06-29 00:08:42 +0300

    Implement the logic of generic guard (the is method) partially, and coverage it by tests. Extract the TestType into a separate file.

--- packages/crypto-kraken-core/src/guards.ts
+++ packages/crypto-kraken-core/src/guards.ts
@@ -7,26 +7,49 @@ export type FieldGuardsMap<T> = {
         ) : (
             T[P] extends object ? (
                 { this?: ((value: any) => value is T[P]) } & FieldGuardsMap<T[P]>
-            ) : FieldGuardsMap<T[P]>
+            ) : never
         )
     );
 };
 
 interface RawFieldGuardsMap {
-    [fieldName: string]: ((value: any) => boolean) | RawFieldGuardsMap;
+    [fieldName: string]: ((value: any) => boolean) | RawFieldGuardsMap | RawObjectFieldsGuardsMap;
+}
+
+type RawObjectFieldsGuardsMap = RawFieldGuardsMap & {
+    this?: (value: any) => boolean;
+};
+
+interface RawArrayFieldsGuardsMap {
+    this?: (value: any) => boolean;
+    every: {
+        [fieldName: string]: RawFieldGuardsMap;
+    };
 }
 
 interface RawFieldsSelector {
     [fieldName: string]: boolean | RawFieldsSelector;
 }
 
-export const checkObject = (
+const isThisOwnerFieldsGuardsMap = (data: any): data is { this: (value: any) => boolean } => data.this;
+const isRawArrayFieldsGuardsMap = (data: any): data is RawArrayFieldsGuardsMap => data && data.every;
+const checkObject = (
     obj: any, fieldGuardMap: RawFieldGuardsMap, checkFields: RawFieldsSelector | boolean = true
 ): boolean => {
     return Object.getOwnPropertyNames(fieldGuardMap)
+        .filter(fieldName => fieldName !== 'this')
         .every(fieldName => {
             const fieldValue = obj[fieldName];
             const guard = fieldGuardMap[fieldName];
+            const thisTrue = !isThisOwnerFieldsGuardsMap(guard) || guard.this(fieldValue);
+            if (!thisTrue)
+                return false;
+
+            if (isRawArrayFieldsGuardsMap(guard))
+                return (fieldValue as any[])
+                    // TODO: replace true
+                    .every(element => checkObject(element, guard.every, true));
+
             if (typeof guard === 'function')
                 return guard(fieldValue);
             return checkObject(
--- packages/crypto-kraken-core/tests/types.spec.ts
+++ packages/crypto-kraken-core/tests/types.spec.ts
@@ -1,4 +1,5 @@
 import { FieldsSelector, FieldsSelectorResult } from 'src';
+import { TestType } from './test-types';
 
 /* These tests are created for type checking only. It shouldn't execute */
 describe.skip('The types:', () => {
@@ -7,32 +8,6 @@ describe.skip('The types:', () => {
             x: boolean;
             y: string;
             z: number;
-        }
-
-        interface TestType {
-            numberField: number;
-            stringField: string;
-            booleanField: boolean;
-            objectField: {
-                numberField: number;
-                stringField: string;
-                objectField1: {
-                    stringField1: string;
-                    stringField2: string;
-                };
-                objectField2: {
-                    stringField1: string;
-                    stringField2: string;
-                };
-            };
-            arrayField: Array<{
-                numberField1: number;
-                numberField2: number;
-                arrayField: Array<{
-                    stringField1: string;
-                    stringField2: string;
-                }>
-            }>;
         }
 
         const instance: TestType = {} as any;
